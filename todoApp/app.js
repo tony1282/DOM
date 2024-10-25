f89b9b74b2c16f6eb1ba05f79d097593c9abac29
antony <antonydelacruzramos@gmail.com>
2024-10-24 20:21:11 -0600

    commit final 2

--- todoApp/app.js
+++ todoApp/app.js
@@ -3,74 +3,78 @@ const divTareas = document.getElementById('tareas');
 const myform = document.querySelector('#myform');
 
 const arrayTareas = localStorage.getItem('tareas')
-                        ? JSON.parse(localStorage.getItem('tareas'))
-                        : [];
-//join a string
-//parse convierte a objeto
+    ? JSON.parse(localStorage.getItem('tareas'))
+    : [];
 
-const addTarea = () =>{
+const addTarea = () => {
     const tarea = inputTarea.value;
     inputTarea.value = '';
-    arrayTareas.push(
-    {
+    arrayTareas.push({
         tarea: tarea,
         estado: 'danger',
         id: Math.floor(Math.random() * 10000)
     });
-
     construyeDivs();
-    localStorage.setItem('tareas', JSON.stringify(arrayTareas))
-
-
-
+    localStorage.setItem('tareas', JSON.stringify(arrayTareas));
 }
-
-
 
 const construyeDivs = () => {
-    divTareas.innerHTML = ''
+    divTareas.innerHTML = '';  
     arrayTareas.forEach(tarea => {
         divTareas.appendChild(createTarea(tarea));
-    })
+    });
 }
 
-
-
-w
+const deleteItem = (e) => {
+    const id = e.target.parentElement.getAttribute('data-id');
+    const index = arrayTareas.findIndex(tarea => tarea.id === Number(id));
+    if (index !== -1) {
+        arrayTareas.splice(index, 1);  
+        construyeDivs();  
+        localStorage.setItem('tareas', JSON.stringify(arrayTareas));
+    }
+}
 
 const createTarea = (tarea) => {
     const div = document.createElement('div');
     const p = document.createElement('p');
     const button = document.createElement('button');
+
     p.innerText = tarea.tarea;
     button.innerText = 'Eliminar';
-    button.addEventListener('click', deleteItem)
+    button.addEventListener('click', deleteItem);
+
     div.appendChild(p);
     div.appendChild(button);
-    div.classList.add('alert');
-    div.classList.add(tarea.estado);
-    div.setAttribute('data-id' , tarea.id)
-    div.addEventListener('click' , changeColor);
+    div.classList.add('alert', tarea.estado);  
+    div.setAttribute('data-id', tarea.id);
+    div.addEventListener('click', changeColor);
+
     return div;
 };
+
 const changeColor = (e) => {
-    const cDiv = e.target;
+    const cDiv = e.currentTarget;  
     const id = cDiv.getAttribute('data-id');
-    const encontrado = arrayTareas.findIndex(tarea => tarea.id === Number(id));
-    arrayTareas[encontrado].estado = "warning";
-    if(cDiv.classList.contains('danger')){
-        cDiv.classList.remove('danger');
-        cDiv.classList.add('warning');
-    } else if (cDiv.classList.contains('warning')){
-        cDiv.classList.remove('warning');
-        cDiv.classList.add('success');
-        arrayTareas[encontrado].estado = "success";
+    const index = arrayTareas.findIndex(tarea => tarea.id === Number(id));
+
+    if (index !== -1) {
+        if (cDiv.classList.contains('danger')) {
+            cDiv.classList.remove('danger');
+            cDiv.classList.add('warning');
+            arrayTareas[index].estado = "warning";
+        } else if (cDiv.classList.contains('warning')) {
+            cDiv.classList.remove('warning');
+            cDiv.classList.add('success');
+            arrayTareas[index].estado = "success";
+        }
+        localStorage.setItem('tareas', JSON.stringify(arrayTareas));  
     }
-    localStorage.setItem('tareas', JSON.stringify(arrayTareas)); 
-    
-}
+};
+
 construyeDivs();
-myform.addEventListener('submit' , (e) =>{
+
+myform.addEventListener('submit', (e) => {
     e.preventDefault();
     addTarea();
-})
+});
